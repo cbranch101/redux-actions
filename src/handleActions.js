b370abc8bf800603086bba646c7e96dd1b096733
Johannes Lumpe <johannes@johanneslumpe.de>
2015-07-07 13:53:54 +0300

    Async actions are now handled by `handleActions`

--- src/handleActions.js
+++ src/handleActions.js
@@ -1,11 +1,16 @@
 import handleAction from './handleAction';
 import ownKeys from './ownKeys';
+import handleAsyncAction from './handleAsyncAction';
 import reduceReducers from 'reduce-reducers';
 
 export default function handleActions(handlers, defaultState) {
-  const reducers = ownKeys(handlers).map(type => handleAction(type, handlers[type]));
+  const reducers = ownKeys(handlers).map(type => {
+    const reducer = handlers[type] 
+    return reducer.begin || reducer.end ?
+      handleAsyncAction(type, reducer) :
+      handleAction(type, reducer);
+  };
   const reducer = reduceReducers(...reducers);
-
   return typeof defaultState !== 'undefined'
     ? (state = defaultState, action) => reducer(state, action)
     : reducer;
